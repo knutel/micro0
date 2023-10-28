98e8cb3219a0323f0d1ef14d6f15a32077a4e903
Knut Eldhuset <knut.eldhuset@gmail.com>
2023-10-28 10:09:55 +0200

    Made instruction classes

--- micro0/emulator/computer.py
+++ micro0/emulator/computer.py
@@ -9,8 +9,80 @@ class Memory:
         self.contents[offset] = value
 
 
+class Instruction:
+    def __init__(self, opcode):
+        self.opcode = opcode
+
+
+class LoadDirect(Instruction):
+    def __init__(self):
+        super().__init__(0x01)
+
+    def tick(self, cpu):
+        if cpu.tick_counter == 2:
+            cpu.index = cpu.memory.read(cpu.pc + 1)
+        elif cpu.tick_counter == 3:
+            cpu.index += cpu.memory.read(cpu.pc + 2) << 8
+        elif cpu.tick_counter == 4:
+            cpu.acc = cpu.memory.read(cpu.index)
+            cpu.pc += 3
+            cpu.tick_counter = 0
+
+
+class StoreDirect(Instruction):
+    def __init__(self):
+        super().__init__(0x02)
+
+    def tick(self, cpu):
+        if cpu.tick_counter == 2:
+            cpu.index = cpu.memory.read(cpu.pc + 1)
+        elif cpu.tick_counter == 3:
+            cpu.index += cpu.memory.read(cpu.pc + 2) << 8
+        elif cpu.tick_counter == 4:
+            cpu.memory.write(cpu.index, cpu.acc)
+            cpu.pc += 3
+            cpu.tick_counter = 0
+
+
+class AddDirect(Instruction):
+    def __init__(self):
+        super().__init__(0x03)
+
+    def tick(self, cpu):
+        if cpu.tick_counter == 2:
+            cpu.index = cpu.memory.read(cpu.pc + 1)
+        elif cpu.tick_counter == 3:
+            cpu.index += cpu.memory.read(cpu.pc + 2) << 8
+        elif cpu.tick_counter == 4:
+            cpu.buffer = cpu.memory.read(cpu.index)
+        elif cpu.tick_counter == 5:
+            cpu.acc = (cpu.acc + cpu.buffer) % 0x100
+            cpu.pc += 3
+            cpu.tick_counter = 0
+
+
+class BranchIfZeroSet(Instruction):
+    def __init__(self):
+        super().__init__(0x04)
+
+    def tick(self, cpu):
+        if cpu.tick_counter == 2:
+            cpu.index = cpu.memory.read(cpu.pc + 1)
+        elif cpu.tick_counter == 3:
+            cpu.index += cpu.memory.read(cpu.pc + 2) << 8
+        elif cpu.tick_counter == 4:
+            if cpu.acc == 0:
+                cpu.pc = cpu.index
+            else:
+                cpu.pc += 3
+            cpu.tick_counter = 0
+
+
 class Cpu:
     def __init__(self, memory: Memory):
+        instructions = [LoadDirect(), StoreDirect(), AddDirect(), BranchIfZeroSet()]
+        self.instructions = {instruction.opcode: instruction for instruction in instructions}
+        assert len(instructions) == len(self.instructions)
         self.pc = 0
         self.acc = 0
         self.index = 0
@@ -22,55 +94,10 @@ class Cpu:
         if self.tick_counter == 0:
             self.tick_counter += 1
         elif self.tick_counter == 1:
-            self.instruction = self.memory.read(self.pc)
+            opcode = self.memory.read(self.pc)
+            self.instruction = self.instructions[opcode]
             self.tick_counter += 1
-        elif self.tick_counter == 2:
-            if self.instruction == 0x01:
-                self.index = self.memory.read(self.pc + 1)
+        else:
+            self.instruction.tick(self)
+            if self.tick_counter > 0:
                 self.tick_counter += 1
-            elif self.instruction == 0x02:
-                self.index = self.memory.read(self.pc + 1)
-                self.tick_counter += 1
-            elif self.instruction == 0x03:
-                self.index = self.memory.read(self.pc + 1)
-                self.tick_counter += 1
-            elif self.instruction == 0x04:
-                self.index = self.memory.read(self.pc + 1)
-                self.tick_counter += 1
-        elif self.tick_counter == 3:
-            if self.instruction == 0x01:
-                self.index += self.memory.read(self.pc + 2) << 8
-                self.tick_counter += 1
-            if self.instruction == 0x02:
-                self.index += self.memory.read(self.pc + 2) << 8
-                self.tick_counter += 1
-            elif self.instruction == 0x03:
-                self.index += self.memory.read(self.pc + 2) << 8
-                self.tick_counter += 1
-            elif self.instruction == 0x04:
-                self.index += self.memory.read(self.pc + 2) << 8
-                self.tick_counter += 1
-        elif self.tick_counter == 4:
-            if self.instruction == 0x01:
-                self.acc = self.memory.read(self.index)
-                self.pc += 3
-                self.tick_counter = 0
-            elif self.instruction == 0x02:
-                self.memory.write(self.index, self.acc)
-                self.pc += 3
-                self.tick_counter = 0
-            if self.instruction == 0x03:
-                self.buffer = self.memory.read(self.index)
-                self.tick_counter += 1
-            if self.instruction == 0x04:
-                if self.acc == 0:
-                    self.pc = self.index
-                else:
-                    self.pc += 3
-                self.tick_counter = 0
-        elif self.tick_counter == 5:
-            if self.instruction == 0x03:
-                self.acc = (self.acc + self.buffer) % 0x100
-                self.pc += 3
-                self.tick_counter = 0
-
